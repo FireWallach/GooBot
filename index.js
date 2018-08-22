433448a7ab1722371fb8d1ce6c729e76d677e78c
firewallach <dylanwallach80@gmail.com>
2018-08-22 15:07:51 -0400

    Removed Rarely Used or Deprecated Commands

--- index.js
+++ index.js
@@ -6,10 +6,7 @@ const bot = new commando.Client();
 bot.registry.registerGroup('random', 'Involves Random Attributes');
 bot.registry.registerGroup('bitsnbobs', 'A command pick\'n\'mix');
 bot.registry.registerGroup('math', 'Math commands, probably just practice for me');
-bot.registry.registerGroup('xiv', 'FFXIV Commands');
-bot.registry.registerGroup('pokemon','Pokemon Commands');
 bot.registry.registerGroup('overwatch', 'Overwatch Commands');
-bot.registry.registerGroup('latin','Latin')
 bot.registry.registerGroup('monhun', 'Monster Hunter');
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname +"/commands")
